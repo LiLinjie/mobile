6fe13287da72596cacbc572fa13d9e4088a69025
LiLinjie <nireus@ixiaopu.com>
2016-08-02 14:35:18 +0800

    1px边框

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
-/**
- * Created by Administrator on 2016/7/11  011.
- */
 var gulp = require("gulp"),
     less = require("gulp-less"),
     watch = require("gulp-watch"),
-    cleanCSS = require("gulp-clean-css");
+    cleanCSS = require("gulp-clean-css"),
+    autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("less", function () {
     watch('src/less/*.less', function () {
         gulp.src('src/less/all.less')
             .pipe(less())
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions', 'Android >= 4.0'],
+                cascsde: true, // 是否美化属性值 默认：true 像这样：
+                // -webkit-transform: rotate(45deg);
+                //         transform: rotate(45deg);
+                remove: true // 是否去掉不必要的前缀 默认：true
+            }))
             .pipe(gulp.dest('src/css/'))
             .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest('dist/css'))
